6ec1f0f0c60a165a05282ff5e42a7b36dbdc5d27
Nikhil Saxena <nikhil.saxena@ericsson.com>
2017-06-07 18:48:05 -0400

    Emnit newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -16,14 +16,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Mike',
-    text: 'hey',
-    createAt: 123
-  });
 
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    // for broadcast use io instead of socket
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   })
 
   socket.on('disconnect', () => {
