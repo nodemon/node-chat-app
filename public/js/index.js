60f4ae9a8c5ea34286dba351fea31687fc399dea
Nikhil Saxena <nikhil.saxena@ericsson.com>
2017-06-08 16:24:04 -0400

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@
 var socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
-  
+
 });
 
 socket.on('disconnect', function () {
@@ -12,4 +12,20 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('New message', message);
+  var li = jQuery('<li></li>'); // creatting an element using jQuery
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+
+// listener method with e event as an argument
+jQuery('#message_form').on('submit', function(e) {
+  e.preventDefault(); // default behavour is page refresh
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  })
 })
--- server/server.js
+++ server/server.js
@@ -3,10 +3,15 @@ const http = require('http');
 const express = require ('express');
 const socketIO = require('socket.io');
 
-var {generateMessage} = require('./message');
+var {generateMessage} = require('./utils/message.js');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 4000;
+
+/*
+  Express and socket.io don't play well together but they are ok with the http module
+  Follwing allows us to take advantage of socket.io and express on the same server
+*/
 
 var app = express();
 var server = http.createServer(app);
@@ -22,22 +27,20 @@ io.on('connection', (socket) => {
 
   // send it to all sockets except the current socket
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'));
-  
-  socket.on('createMessage', (message) => {
+
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
 
     // for broadcast use io instead of socket
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    })
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from server');
   })
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
   })
 })
+
 
 server.listen(port, ()=> {
   console.log(`server up on port ${port}`);
