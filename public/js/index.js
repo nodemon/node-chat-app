eb52af9715ed434076f6d618dc506bdbc606a827
Nikhil Saxena <nikhil.saxena@ericsson.com>
2017-06-08 22:37:44 -0400

    Add css to chat page

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,8 @@
 // for compatibility with old browsers dont use arrow function on client js
 
+// Forms are old school . onsubmit does a full page refresh and adds data to the url as a query string -->
+// We override the default behaviour using jquery -->
+
 var socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
@@ -12,7 +15,7 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('New message', message);
-  var li = jQuery('<li></li>'); // creatting an element using jQuery
+  var li = jQuery('<li></li>'); // creating an element using jQuery
   li.text(`${message.from}: ${message.text}`);
 
   jQuery('#messages').append(li);
@@ -31,28 +34,34 @@ socket.on('newLocationMessage', function(message) {
 // listener method with e event as an argument
 jQuery('#message_form').on('submit', function(e) {
   e.preventDefault(); // default behavour is page refresh
+
+  var messageTextBox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function() {
-
+    messageTextBox.val(''); // clear
   })
 });
 
 
 var locationButton = jQuery('#send_location');
-//listener below is same as ('#send_location').on() .. but will query DOM structure again
+//listener below is same as ('#send_location').on() .. but will not query DOM structure again
 locationButton.on('click', function() {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location ...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude,
     })
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   });
 
