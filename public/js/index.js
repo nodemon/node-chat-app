a5ae59bfcafe756fd84eff5ba86f2bb568ef7942
Nikhil Saxena <nikhil.saxena@ericsson.com>
2017-06-09 10:56:30 -0400

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -15,21 +15,42 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>'); // creating an element using jQuery
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  var template = jQuery('#message-template').html();  // html() returns markup inside
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 
-  jQuery('#messages').append(li);
+
+  // var formattedTime = moment(message.createdAt).format('h:mm a');
+  // var li = jQuery('<li></li>'); // creating an element using jQuery
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  //
+  // jQuery('#messages').append(li);
 });
 
 
 socket.on('newLocationMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>') // target="_blank for opening in new tab
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-message-template').html();  // html() returns markup inside
+  var html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
+
+
+
+
+  // var li = jQuery('<li></li>');
+  // var a = jQuery('<a target="_blank">My current location</a>') // target="_blank for opening in new tab
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
 })
 
 // listener method with e event as an argument
